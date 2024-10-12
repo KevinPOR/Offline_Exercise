ce8b414614358c4dbe75c589d071e9ebe12f8301
KarimKousa <61150696+KevinPOR@users.noreply.github.com>
2024-10-12 20:53:19 +0100

    Add files via upload

---
+++ QueueTest/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/test_queue.cpp.o"
+  "CMakeFiles/runTests.dir/test_queue.cpp.o.d"
+  "librunTests.dll.a"
+  "runTests.exe"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ QueueTest/build/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/test_queue.cpp.o"
+  "runTests.pdb"
+  "runTests.exe"
+  "librunTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
